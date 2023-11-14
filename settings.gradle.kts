f3211002aecfa71fa14db6e3739fea50780cae16
mbialowas10 <mbialowas@rrc.ca>
2023-11-14 09:16:34 -0600

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BeerOnAndroid2023Demo"
+include(":app")
+ 
