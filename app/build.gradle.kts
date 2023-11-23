207b8c059a10b39f2eafbca47520d70defb46e03
mbialowas10 <mbialowas@rrc.ca>
2023-11-23 10:20:18 -0600

    Lazy List implemented for beers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,8 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.9.3")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 
+    // coil
+    implementation("io.coil-kt:coil-compose:1.3.2")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
--- app/src/main/java/com/mbialowas/beeronandroid2023demo/screens/Beers.kt
+++ app/src/main/java/com/mbialowas/beeronandroid2023demo/screens/Beers.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,20 +25,30 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
 import com.mbialowas.beeronandroid2023demo.R
 import com.mbialowas.beeronandroid2023demo.api.BeersManager
+import com.mbialowas.beeronandroid2023demo.model.BeerItem
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 
 @Composable
 fun Beers(beersManager: BeersManager) {
-    BeerCard()
-    Log.i("API_Response", beersManager.beersResponse.value.toString())
+    //BeerCard()
+    val beers = beersManager.beersResponse.value
+
+    //Log.i("API_Response", beersManager.beersResponse.value.toString())
+
+    LazyColumn{
+        items(beers){beer ->
+            BeerCard(beerItem = beer)
+        }
+    }
 }
 
 @Composable
-fun BeerCard (){
+fun BeerCard (beerItem: BeerItem){
     // parameters
     Column(
         modifier = Modifier
@@ -53,20 +65,21 @@ fun BeerCard (){
                 modifier = Modifier
                     .size(50.dp)
                     .clip(CircleShape),
-                painter = painterResource(id = R.drawable.ic_launcher_background),
+                //painter = painterResource(id = R.drawable.ic_launcher_background),
+                painter = rememberImagePainter(data = beerItem.image),
                 contentDescription = "Beer"
             )
             Column {
                 Text(
                     color = Color.White,
-                    text = "A Beer ....",
+                    text = beerItem.name,
                     modifier = Modifier
                         .padding(top=8.dp,end=8.dp),
                     style= TextStyle(fontSize= 24.sp),
                     maxLines=1
                 )
                 Text(
-                    text= "2.33",
+                    text= beerItem.price,
                     modifier = Modifier.padding(end=8.dp),
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
@@ -77,7 +90,7 @@ fun BeerCard (){
 
                     Row{
                         Text(
-                            text = "Average Vote: ",
+                            text = "Average Vote: " + BigDecimal(beerItem.rating.average).setScale(2,RoundingMode.HALF_UP).toString() + "/5",
                             modifier = Modifier.padding(end = 8.dp),
                             maxLines = 1,
                             overflow = TextOverflow.Ellipsis,
@@ -85,7 +98,7 @@ fun BeerCard (){
                             color = Color.White
                         )
                         Text(
-                            text = "# of Reviews ",
+                            text = "# of Reviews " + beerItem.rating.toString(),
                             modifier = Modifier.padding(end = 8.dp),
                             maxLines = 1,
                             overflow = TextOverflow.Ellipsis,
