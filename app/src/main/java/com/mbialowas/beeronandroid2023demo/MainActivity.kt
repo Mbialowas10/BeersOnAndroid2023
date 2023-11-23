4b4bd6f35f2c34d8dbc422bc52452ad38fe64270
mbialowas10 <mbialowas@rrc.ca>
2023-11-23 16:29:45 -0600

    Finished Primary Navigation

--- app/src/main/java/com/mbialowas/beeronandroid2023demo/MainActivity.kt
+++ app/src/main/java/com/mbialowas/beeronandroid2023demo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -24,6 +25,8 @@ import com.mbialowas.beeronandroid2023demo.screens.Beers
 import com.mbialowas.beeronandroid2023demo.ui.theme.BeerOnAndroid2023DemoTheme
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +42,7 @@ class MainActivity : ComponentActivity() {
                     // fetch our beers from the api
                     val beersManager: BeersManager = BeersManager()
 
-                    Beers(beersManager)
+                    //Beers(beersManager)
 
                     val navController = rememberNavController()
 
--- app/src/main/java/com/mbialowas/beeronandroid2023demo/navigation/BottomNavBar.kt
+++ app/src/main/java/com/mbialowas/beeronandroid2023demo/navigation/BottomNavBar.kt
@@ -1,13 +1,16 @@
 package com.mbialowas.beeronandroid2023demo.navigation
 
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.mbialowas.beeronandroid2023demo.navigation.BottomNavItem.About.icon
 
 
 @Composable
@@ -25,5 +28,28 @@ fun BottomNavBar(navController: NavController){
         val currentRoute = navBackStackEntry?.destination?.route
 
         // loop through navitems to wire up our navigation system
+        items.forEach { item ->
+            NavigationBarItem (
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route){
+                        popUpTo(navController.graph.startDestinationId){
+                            saveState = true
+                        }
+                        // avoid multiple copies of the same destination
+                        launchSingleTop = true
+                        // restore state when reselection of a previously selected item
+                        restoreState = true
+                    }
+                },
+                    icon ={
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = "Always a good idea to fill for screen readers"
+                        )
+                }
+            )
+        }
+
     }
 }
